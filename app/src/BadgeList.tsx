87c1e4884c6b535f2eaed2694ce2edc49087f91a
deenu <19dworrall@stbarts.co.uk>
2025-05-23 11:33:49 +0100

    added a way to search for badges by looking up who they were awarded to

--- app/src/BadgeList.tsx
+++ app/src/BadgeList.tsx
@@ -1,15 +1,29 @@
 import { Badge } from './App';
-
+import { useState } from 'react';
 interface Props {
   badges: Badge[];
 }
 
 function BadgeList({ badges }: Props) {
+    const[form,setForm] =useState("")
+    const filteredBadges = badges.filter(badge =>
+    badge.awardedTo.toLowerCase().includes(form.toLowerCase())
+  );
+
   return (
     <div>
       <h2>🏅 Badges</h2>
+      <form>
+      <label>search by awardedTo:
+        <input type="text"
+         value= {form}
+         onChange={(e) => setForm(e.target.value)}
+         placeholder="search" required
+        />
+      </label>
+      </form>
       <ul>
-        {badges.map(badge => (
+        {filteredBadges.map(badge => (
           <li key={badge.id}>
             <strong>{badge.title}</strong> - {badge.description} (Awarded to: {badge.awardedTo})
           </li>
