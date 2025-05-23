3a37132f451c47fcc3638e5d07c4193193130b8b
KRISHNA <raturiryan@gmail.com>
2025-05-23 11:27:45 +0100

    Added badge

--- app/src/BadgeForm.tsx
+++ app/src/BadgeForm.tsx
@@ -7,13 +7,28 @@ interface Props {
 function BadgeForm({ onAdd }: Props) {
   const [form, setForm] = useState({ title: '', description: '', awardedTo: '' });
 
+  const[error, setError] = useState('');
+
+
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const allFilled = Object.values(form).every((val) => val.trim() !== '');
+
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    onAdd(form);
+    if (!allFilled) {
+        setError('Field is required')
+    } else {
+        setError('');
+        console.log('Submitted:', name)
+
+        }
+
+    onAdd(form)
     setForm({ title: '', description: '', awardedTo: '' });
   };
 
@@ -22,9 +37,17 @@ function BadgeForm({ onAdd }: Props) {
       <input name="title" value={form.title} onChange={handleChange} placeholder="Title" required />
       <input name="description" value={form.description} onChange={handleChange} placeholder="Description" required />
       <input name="awardedTo" value={form.awardedTo} onChange={handleChange} placeholder="Awarded To" required />
-      <button type="submit">Add Badge</button>
+
+      {error && <p style={{color:'red'}}>{error}</p>}
+      <button type="submit" disabled={!allFilled}>Add Badge</button>
     </form>
   );
 }
 
+
+
+
 export default BadgeForm;
+
+
+//disabled={!allFilled}
